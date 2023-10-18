e7400f8560523fcf41ea49d3a9ba46714716f2db
Shovon96 <fakhruddinshovon@gmail.com>
2023-10-19 03:16:04 +0600

    remove node modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
@@ -9,11 +9,8 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-// autoMotive
-// XhuI7VEnksI5ChBr
 
-
-const uri = "mongodb+srv://autoMotive:XhuI7VEnksI5ChBr@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
